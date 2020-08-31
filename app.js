6eef02ab3786121fcb8f5955fb3fc5b85743cb0f
DESKTOP-35TADGG\alaga <alagaesia2004@hotmail.com>
2020-08-31 17:32:49 +0200

    tenemos que lanzar la libreria dotenv en algun punto la nuestra aplicacion, lo antes posible para que luego dentro de la variable de entorno procces.env tengamos todos los datos que hemos guardado en el fichero de entorno

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express(); /* llamada a la funcion de express */
 
+require('dotenv').config();
+
 app.get('/', (req, res) => {
     res.send('Hola servidor chulo');
 });
