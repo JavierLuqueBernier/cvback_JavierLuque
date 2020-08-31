0ce7ed21a9f663d10ad82febd6813108af0cda09
DESKTOP-35TADGG\alaga <alagaesia2004@hotmail.com>
2020-08-31 17:01:10 +0200

    Primera peticion al servidor basica para ver que funciona y que recibe peticiones correctamente

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express(); /* llamada a la funcion de express */
 
-
+app.get('/', (req, res) => {
+    res.send('Hola servidor');
+});
 
 app.listen(3000, () => {
     console.log('Servidor escuchando en puerto 3000');
-})
+});
