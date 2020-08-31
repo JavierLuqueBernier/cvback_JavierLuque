9cb7359d158a57743ff88788797948d4564df4aa
DESKTOP-35TADGG\alaga <alagaesia2004@hotmail.com>
2020-08-31 17:24:59 +0200

    Hacemos prueba de que nodemon está funcionando

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); /* llamada a la funcion de express */
 
 app.get('/', (req, res) => {
-    res.send('Hola servidor');
+    res.send('Hola servidor chulo');
 });
 
 app.listen(3000, () => {
