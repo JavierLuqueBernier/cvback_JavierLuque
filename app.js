cf179765287ea68a069f6d668742df489acee633
DESKTOP-35TADGG\alaga <alagaesia2004@hotmail.com>
2020-08-31 16:52:54 +0200

    Ponemos a escuchar al servidor en el puerto 3000 aunque mas adelante tendremos que modificarlo para escuchar el puerto que nos indique heroku

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const express = require('express');
+const app = express(); /* llamada a la funcion de express */
+
+
+
+app.listen(3000, () => {
+    console.log('Servidor escuchando en puerto 3000');
+})
