ab388f8fab67fc72964d18839940382eecb990c8
DESKTOP-35TADGG\alaga <alagaesia2004@hotmail.com>
2020-09-02 21:02:12 +0200

    Debido a problemas con heroku, acabamos utilizando el servicio de mongodb atlas, por lo que modificamos la direccion url

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const urlMongo = 'redis://h:p74a9fd13efef9cdd78c037427107133da8cfa4372639f75d06dcb4a46f03e6ab@ec2-54-220-24-58.eu-west-1.compute.amazonaws.com:27539';
+const urlMongo = 'mongodb+srv://javier_luque:tarragona@cluster0.pr5sz.mongodb.net/test';
 
 const config = {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
 };
 mongoose.connect(urlMongo, config);
