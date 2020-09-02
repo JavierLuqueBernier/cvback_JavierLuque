de6eddf533d463d91d8e8a7a8812a07dbf6780db
DESKTOP-35TADGG\alaga <alagaesia2004@hotmail.com>
2020-09-02 18:06:40 +0200

    Debido a que mongoose no se actualiza demasiado, tiene algunas partes que aparecen como deprecadas por lo que debemos meter ciertas lineas de configuracion adicionales y pedirle a la conexion con mongoose que use tanto la urlMongo como la configuracion adicional

--- db.js
+++ db.js
@@ -2,4 +2,8 @@ const mongoose = require('mongoose');
 
 const urlMongo = 'redis://h:p74a9fd13efef9cdd78c037427107133da8cfa4372639f75d06dcb4a46f03e6ab@ec2-54-220-24-58.eu-west-1.compute.amazonaws.com:27539';
 
-mongoose.connect(urlMongo);
+const config = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+mongoose.connect(urlMongo, config);
