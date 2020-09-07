d2ff6714a96b268ca8d0fb67cc72088171de4fcc
DESKTOP-35TADGG\alaga <alagaesia2004@hotmail.com>
2020-09-07 16:55:43 +0200

    Requerimos bodyParser para poder utilizarlo, asi como el codigo que indica la documentacion necesaria para su funcionamiento

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express(); /* llamada a la funcion de express */
 
 require('dotenv').config();
 require('./db');
 
 const apiRouter = require('./routes/api');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', apiRouter);
 
