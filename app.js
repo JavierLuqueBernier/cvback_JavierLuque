0c85d86b651aeb2da9bf948dffc665b3d2cb309d
DESKTOP-35TADGG\alaga <alagaesia2004@hotmail.com>
2020-09-07 17:06:15 +0200

    Dado que no vamos a utilizar finalmente body-parser borramos todas las lineas de uso asi como el requerimiento

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express(); /* llamada a la funcion de express */
 
 require('dotenv').config();
 require('./db');
 
-const apiRouter = require('./routes/api');
-
-/* app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); */
+const apiRouter = require('./routes/api'); 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
