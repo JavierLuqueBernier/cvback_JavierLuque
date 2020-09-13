6f4de6a8cbbd3beeec3c3c26bd15850105f0b5a4
DESKTOP-35TADGG\alaga <alagaesia2004@hotmail.com>
2020-09-13 23:58:11 +0200

    Dentro de cada token habrá un payload que es generalmente donde se guardan datos del mismo, tales como el nombre, el momento de creacion y el tiempo de expiracion. Esto ultimo se hace con la libreria moment y se utiliza Unix para transformarlo en milisegundos. En el caso del momento de creacion nos dara los milisegundos que han pasado hasta este preciso momento desde el dia 1 de enero de 1970.

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
+const moment = require('moment');
+const jwt = require('jwt-simple');
 
 const apiRouterProyectos = require ('./api/proyectos');
 
 router.use('/proyectos', apiRouterProyectos);
 
 router.get('/token', (req, res) => {
-
+    let payload = {
+        usuario: 'Javier',
+        createAt: moment().unix(),
+        expiredAt: moment().add(5, 'minutes').unix()
+    };
 });
 
 module.exports = router;
