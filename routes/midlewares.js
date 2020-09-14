6ca49815f23bc8b6b02b33571df947621f7ffcd1
DESKTOP-35TADGG\alaga <alagaesia2004@hotmail.com>
2020-09-14 22:42:13 +0200

    Creamos una funcion en el middleware la cual comprueba si las peticiones que nos lleguen cuentan con un header, en el cual va la información del token, el usuario y el tiempo de expiracion y nos responde con "Deben incluir la cabecera access-token" en caso de que no lo lleven.

---
+++ routes/midlewares.js
@@ -0,0 +1,9 @@
+exports.checkToken = (req, res, next) => {
+    if(!req.headers['access-token']) {
+        return res.status(403).json({ error: 'Debes incluir la cabecera access-token'});
+    }
+
+    const token = req.headers['access-token'];
+
+    next();
+}
