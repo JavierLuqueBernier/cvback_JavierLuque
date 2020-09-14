caf0e0419cf4c0178e1f19d7a4a5e12bc74c05c8
DESKTOP-35TADGG\alaga <alagaesia2004@hotmail.com>
2020-09-14 22:43:48 +0200

    Llamamos a las librerias moment y JWT para revisar que el tiempo no haya expirado la primera y para decodificar la informacion la segunda

--- routes/midlewares.js
+++ routes/midlewares.js
@@ -1,3 +1,6 @@
+const jwt = require('jwt-simple');
+const moment = require('moment');
+
 exports.checkToken = (req, res, next) => {
     if(!req.headers['access-token']) {
         return res.status(403).json({ error: 'Debes incluir la cabecera access-token'});
