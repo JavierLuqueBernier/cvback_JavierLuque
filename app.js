29e8ee6649c5d8c649406c3da5fa8d97d897ada1
DESKTOP-35TADGG\alaga <alagaesia2004@hotmail.com>
2020-09-22 13:15:46 +0200

    Generamos las rutas para que todas las peticiones que me llegen con le ruta localhost:3000/proyectos, se redireccionen a routes/proyectos.js y se gestionen desde allí

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const app = express(); /* llamada a la funcion de express */
 require('dotenv').config();
 require('./db');
 
-const apiRouter = require('./routes/api'); 
+const apiRouter = require('./routes/api');
+const proyectosRouter = require('./routes/proyectos');
 
 // Template Engine Setup
 app.engine('.hbs', exphbs({ extname: '.hbs' }));
@@ -16,10 +17,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-app.get('/test', (req, res) => {
-    res.render('prueba');
-})
-app.use('/api', apiRouter);
+
+app.use('/api', apiRouter); /* Aqui estamos diciendo:"si me llega una peticion con api me la mandas a routes/api" */
+app.use('/proyectos', proyectosRouter); /* Aqui estamos diciendo:"si me llega una peticion con proyectos me la mandas a routes/proyectos" */
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
