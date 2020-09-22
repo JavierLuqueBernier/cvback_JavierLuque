258f3fb9ebf34281ef3ae75ba667138ca0113814
DESKTOP-35TADGG\alaga <alagaesia2004@hotmail.com>
2020-09-22 11:50:02 +0200

    Requerimos express-handlebars para crear vistas en el backend y escribimos las lineas necesarias para que el backend nos reconozca los archivos formato ".hbs",  además de indicar la ruta donde debe buscar los archivos donde vamos a crear las vistas del backend para el proyecto

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const exphbs = require('express-handlebars');
 const app = express(); /* llamada a la funcion de express */
 
 require('dotenv').config();
@@ -7,10 +8,17 @@ require('./db');
 
 const apiRouter = require('./routes/api'); 
 
+// Template Engine Setup
+app.engine('.hbs', exphbs({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+app.get('/test', (req, res) => {
+    res.render('prueba');
+})
 app.use('/api', apiRouter);
 
 const PORT = process.env.PORT || 3000;
