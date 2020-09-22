13dc12748abbf290e2e29898fca348b795ff50cd
DESKTOP-35TADGG\alaga <alagaesia2004@hotmail.com>
2020-09-22 21:13:47 +0200

    Pequeña modificacion debida a que handlewars daba error al ilustrar los proyectos si usamos mongoose, por lo que no vemos obligados a implementar esos cambios y usar el metodo .lean() para transformar los objetos de mongoose en json objects

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -3,8 +3,8 @@ const router = require('express').Router();
 const Proyecto = require('../models/proyecto');
 
 router.get('/', async (req, res) => {
-    const proyectos = await Proyecto.find();
-    res.render('proyectos/index', { proyectos });
+    const proyectos = await Proyecto.find().lean();
+    res.render('proyectos/index', { proys: proyectos });
 });
 
 module.exports = router;
