4c51e69da1fca0b38a99cb339725d6bdff1057b6
DESKTOP-35TADGG\alaga <alagaesia2004@hotmail.com>
2020-09-22 13:16:23 +0200

    Generamos la primera funcion para traernos todos los proyectos

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
 
+const Proyecto = require('../models/proyecto');
 
+router.get('/', async (req, res) => {
+    const proyectos = await Proyecto.find();
+    res.render('proyectos/index', { proyectos });
+});
 
 module.exports = router;
